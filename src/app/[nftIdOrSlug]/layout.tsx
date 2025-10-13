6d497274a4a17e34aff38fab9490a83518323ba9
basya01 <arsen@swarm.com>
2025-10-13 19:45:22 +0300

    update

--- src/app/[nftIdOrSlug]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react'
-import ReadLayout from 'src/components/common/Layout/ReadLayout'
-
-const Layout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <ReadLayout>{children}</ReadLayout>
-}
-
-export default Layout
